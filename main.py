78ae42fb4bcf801161fc18c633d4b585c7ff7fce
SarahNGardner <sarahngardner@gmail.com>
2023-11-05 22:16:18 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+import argparse
+import boto3
+
+# Constants
+POLL_INTERVAL_SECONDS = 0.1  # 100 ms
+BUCKET_2_NAME = "usu-cs5260-red-requests"
+BUCKET_3_NAME = "usu-cs5260-red-web"
+DYNAMODB_TABLE_NAME = "your-dynamodb-table-name"
+
+def process_widget_request(request):
+
+
+def read_widget_request():
+
+
+def main():
+
+
+main()
