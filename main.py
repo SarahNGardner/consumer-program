1ec3483adb0f154f938c7e6352774e45ee965b03
SarahNGardner <sarahngardner@gmail.com>
2023-11-05 22:46:14 -0700

    create basic request function

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ BUCKET_3_NAME = "usu-cs5260-red-web"
 DYNAMODB_TABLE_NAME = "your-dynamodb-table-name"
 
 def process_widget_request(request):
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(request, BUCKET_3_NAME, request)
 
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+
+    s3_client.delete_object(Bucket=BUCKET_2_NAME, Key=request)
 
 def read_widget_request():
 
